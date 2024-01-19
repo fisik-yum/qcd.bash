92b5c3887e54e9de5508aaec0cfc31c196de6341
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2024-01-18 19:07:31 -0800

    inital commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# qcd
+
+`qcd.bash` (short for "quick-cd") is a bash script that "replaces" the traditional `cd` command. It allows you quickly cd to a tagged directory using the `qcd` command, while attempting to preserve `cd`. Arguments to `cd` are passed transparently, and it prompts the user to create a tag when a directory is visited often during a shell session.
+
+qcd was inspired by [warpdrive-go](https://github.com/quackduck/warpdrive-go)
+
+#### Installation
+
+First source the file in your rcfile and call the initialization function
+`source /path/to/qcd.bash`
+
+Then create an alias mapping `cd` to `qcd-transparent`
+`alias cd="qcd-transparent"`
+
+### Configuration
+Behavior can be modified after sourcing `qcd`.
+```
+QCD_MINIMUMLINES=8 # "popularness" threshold
+QCD_HOME="$HOME" # home for the qcd data file
+```
+
+##### License
+qcd.bash is licensed under the GNU AGPLv3
---
+++ qcd.bash
@@ -0,0 +1,124 @@
+# qcd.bash - a somewhat smarter cd command
+#    Copyright (C) 2024  fisik_yum
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#!/bin/bash
+
+# default configuration - can be overriden after sourcing in .bashrc
+export QCD_MINIMUMLINES=8
+export QCD_HOME="$HOME"
+
+export dirlist="" # don't override this!
+
+qcd-transparent ()
+{
+    cd "$@"
+    if ! [[ "$(pwd)" == "$QCD_HOME" ]]; then
+        dirlist+='"'$(pwd)'"'" "
+        if ! [[ "$1" = .. ]]; then
+            __qcd_check
+        fi
+    fi
+
+}
+
+qcd ()
+{ 
+    tdir=$(__qcd_rcread $1)
+    if ! [[ $tdir == "" ]]; then
+        cd $tdir
+    else
+        echo "qcd: $1: No such tag"
+    fi
+}
+
+qcd-add () #$dir
+{
+    if [[ $1 == "." ]]; then
+        __qcd_tagprompt $(pwd)
+    elif [[ -z "${1// }" || $1 == ".." ]]; then
+        while true; do
+            read -e -p "Enter directory: " loc
+            if [[ -z "${loc// }" ]]; then
+                echo "Please enter a valid directory."
+            else
+                __qcd_tagprompt $loc
+                break
+            fi
+        done
+    else
+        __qcd_tagprompt $1
+    fi
+}
+
+qcd_init ()
+{
+    if ! [ -f "$QCD_HOME/.qcdrc" ]; then
+        touch "$QCD_HOME/.qcdrc"
+    fi
+}
+
+__qcd_check ()
+{
+    num=$(echo -n $dirlist | grep -Fo '"'$(pwd)'"' | wc -l)
+    if [[ $num -eq QCD_MINIMUMLINES ]]; then
+        read -p "qcd: Create tag [yN]?" yn
+            case $yn in
+                [Yy]* )  __qcd_tagprompt $(pwd) ; return 0;;
+                [Nn]* ) return 1;;
+                * ) return 1;;
+            esac
+    fi
+}
+
+__qcd_tagprompt () #$value
+{
+    while true; do
+        read -p "Enter tag name: " name
+        if [[ -z "${name// }" || ! $(__qcd_rcread $name) == "" ]]; then
+            echo "Please enter a valid name. The name specified may already be in use."
+        else
+            __qcd_rcwrite $name $1
+            break
+        fi
+    done
+
+}
+
+__qcd_rcwrite () #$key $value
+{
+    if [[ $(__qcd_rcread $1) == "" ]]; then
+        echo -e "$1" "$2"  >> "$QCD_HOME/.qcdrc"
+    else
+        echo "qcd: $1: Tag already exists"
+    fi
+
+}
+
+
+__qcd_rcread () #$key
+{
+    input="$QCD_HOME/.qcdrc"
+    while IFS= read -r line
+    do
+        key=${line%% *}
+        value=${line#* }
+        if [[ $1 == $key ]]; then
+            echo $value
+            break 
+        fi
+    done < "$input"
+    echo ""
+}
